204908d120f5919103f04d1a1aab778c513a79f9
torte478 <torte478@gmail.com>
2020-06-27 19:53:26 +0300

    feature (Images): add calculating of average color

--- src/JigsawService/Images.cs
+++ src/JigsawService/Images.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.Extensions.Logging;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
 using JigsawService.Extensions;
 using JigsawService.Templets;
-using SixLabors.ImageSharp.Processing;
-using System.Linq;
 
 namespace JigsawService
 {
@@ -31,9 +33,14 @@ namespace JigsawService
 
         public Image BuildPreview(Image origin, Templet templet)
         {
-            var preview = origin.Clone(_ => _.Resize(prototype.size.Width, prototype.size.Height));
-
             var size = new Size(
+                prototype.size.Width - prototype.size.Width % prototype.pieces.Width,
+                prototype.size.Height - prototype.size.Height % prototype.pieces.Height);
+            var preview = origin
+                            .CloneAs<Rgba32>()
+                            .Clone(_ => _.Resize(size));
+
+            var factor = new Size(
                             preview.Width / prototype.pieces.Width, 
                             preview.Height / prototype.pieces.Height);
             var pieces = Enumerable
@@ -41,10 +48,44 @@ namespace JigsawService
                             .SelectMany(i => Enumerable
                                              .Range(0, prototype.pieces.Width)
                                              .Select(j => (
-                                             j * size.Width,
-                                             i * size.Height)));
-
+                                             j * factor.Width,
+                                             i * factor.Height)));
+            FillPieces(preview, pieces, factor);
             return preview;
+        }
+
+        private void FillPieces(
+                        Image<Rgba32> image, 
+                        IEnumerable<(int x, int y)> pieces, 
+                        Size size)
+        {
+            foreach (var piece in pieces)
+            {
+                var r = 0;
+                var g = 0;
+                var b = 0;
+                var total = 0;
+                for (var x = 0; x < size.Width && piece.x + x < image.Width; ++x)
+                for (var y = 0; y < size.Height && piece.y + y < image.Height; ++y)
+                {
+                    var pixel = image[piece.x + x, piece.y + y];
+                    r += pixel.R * pixel.R;
+                    g += pixel.G * pixel.G;
+                    b += pixel.B * pixel.B;
+                    ++total;
+                }
+                var color = new Rgba32(
+                    (byte)Math.Sqrt(r / total),
+                    (byte)Math.Sqrt(g / total),
+                    (byte)Math.Sqrt(b / total));
+
+                for (var y = 0; y < size.Height && piece.y + y < image.Height; ++y)
+                {
+                    var span = image.GetPixelRowSpan(piece.y + y);
+                    for (var x = 0; x < size.Width && piece.x + x < image.Width; ++x)
+                        span[piece.x + x] = color;
+                }
+            }
         }
 
         public Maybe<Image, string> Load(byte[] image)
