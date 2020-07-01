ab87f239464d317dbd11159195a028ab60f817a3
torte478 <torte478@gmail.com>
2020-07-01 20:53:53 +0300

    feature (GatewayApi): add web api project

---
+++ src/GatewayApi/Controllers/JigsawController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GatewayApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JigsawController : ControllerBase
+    {
+        [HttpGet("{number}")]
+        public string Get(int number)
+        {
+            return $"Hello, world! = {number}";
+        }
+    }
+}
